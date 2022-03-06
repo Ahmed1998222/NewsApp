0c7296ec390fcbc412dc2d5b5760f5b408a34878
Ahmed <38440624+Ahmed1998222@users.noreply.github.com>
2022-03-06 10:33:39 +0300

    feat : page view container

---
+++ src/components/common/pageView/PageView.tsx
@@ -0,0 +1,48 @@
+import theme from '../../../assets/theme/theme';
+import { selectIsDark } from '../../../store/slices/themSlice';
+import React, { FC } from 'react';
+import {
+  View,
+  ScrollView,
+  StatusBar,
+  SafeAreaView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import { useSelector } from 'react-redux';
+import styles from './styles';
+
+
+export const PageView = ({ style, scrollEnabled, children }) => {
+  const isDark = useSelector(selectIsDark);
+
+  return (
+    <SafeAreaView
+      style={[
+        styles.safeAreaView,
+        isDark ? styles.darkBackground : styles.lightBackground,
+      ]}>
+      <StatusBar
+        barStyle={isDark ? 'light-content' : 'dark-content'}
+        backgroundColor={theme.colors.white}
+        translucent
+      />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        keyboardVerticalOffset={Platform.OS === 'ios' ? 15 : 0}
+        style={styles.flexGrowOne}>
+        {scrollEnabled ? (
+          <ScrollView
+            contentContainerStyle={[styles.scrollViewContainer, style]}
+            style={styles.container}
+            showsVerticalScrollIndicator={false}
+          >
+            {children}
+          </ScrollView>
+        ) : (
+          <View style={[styles.container, style]}>{children}</View>
+        )}
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+};
