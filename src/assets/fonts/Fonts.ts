b0b0d9fe25a8fab546e377d2eb0c563e4d6a8089
Ahmed <38440624+Ahmed1998222@users.noreply.github.com>
2022-03-06 09:59:59 +0300

    feat : color and fonts

---
+++ src/assets/fonts/Fonts.ts
@@ -0,0 +1,7 @@
+export const FONT_FAMILIES = {
+  regular: 'Klarheit-Grotesk-Regular',
+  medium: 'Klarheit-Grotesk-Medium',
+  bold: 'Klarheit-Grotesk-Bold',
+  italic: 'Klarheit-Grotesk-Italic',
+  italicBold: 'Klarheit-Grotesk-Bold-Italic',
+};
---
+++ src/assets/theme/theme.ts
@@ -0,0 +1,31 @@
+import {FONT_FAMILIES}from '../fonts/Fonts'
+export const COLORS = {
+  primary: '#572451',
+  secondary: '#82518a',
+  white: '#F2F3F7',
+  black: '#000000',
+   error: '#F95A5A',
+  gray: '#A1A4B2',
+  lightGray: '#DADADA',
+  darkGray: '#606978',
+  gradient: {
+    topColor: '#1F6898',
+    bottomColor: '#FFFFFF',
+  },
+  lightBlue: '#1079EF',
+  darkBlue: '#002147',
+  background: {
+    light: '#E5E5E5',
+    dark: '#013570',
+  },
+  bottomBarBackgrounds: {
+    dark: '#002147',
+    light: '#F2F3F7',
+  },
+};
+
+ export default {
+  colors: COLORS,
+  fontFamily: FONT_FAMILIES,
+};
+
